c0823be1aa7167c2f41dbd11120b9e8bee91b298
Will Norton <norton402@morrisville.edu>
2021-02-27 12:47:01 -0500

    Item video completed, added "Yellow Stick" item

---
+++ WillsMod/src/main/java/com/wnorton/willsmod/events/RegistryEvents.java
@@ -0,0 +1,30 @@
+package com.wnorton.willsmod.events;
+
+import org.apache.logging.log4j.Logger;
+
+import com.wnorton.willsmod.Main;
+import com.wnorton.willsmod.lists.ItemList;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+public class RegistryEvents {
+	public static final Logger LOGGER = Main.LOGGER;
+	public static final String MOD_ID = Main.MOD_ID;
+	
+	@SubscribeEvent
+	public static void registerItems(final RegistryEvent.Register<Item> event) {
+		event.getRegistry().registerAll(
+				ItemList.tutorial_item = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("tutorial_item"))
+		);
+	}
+	
+	public static ResourceLocation location(String name) {
+		return new ResourceLocation(MOD_ID, name);
+	}
+}
---
+++ WillsMod/src/main/java/com/wnorton/willsmod/lists/ItemList.java
@@ -0,0 +1,9 @@
+package com.wnorton.willsmod.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+	public static Item tutorial_item;
+	
+	
+}
