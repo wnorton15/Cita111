463c52e9bee5a9a6c51f94cc3424e17a8894cf02
Will Norton <norton402@morrisville.edu>
2021-03-18 21:14:10 -0400

    Added Stairs

--- WillsMod/src/main/java/com/wnorton/willsmod/events/RegistryEvents.java
+++ WillsMod/src/main/java/com/wnorton/willsmod/events/RegistryEvents.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import com.wnorton.willsmod.Main;
 import com.wnorton.willsmod.lists.BlockList;
 import com.wnorton.willsmod.lists.ItemList;
+import com.wnorton.willsmod.objects.blocks.CustomStairsBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
@@ -28,7 +29,8 @@ public class RegistryEvents {
 		event.getRegistry().registerAll(
 				ItemList.tutorial_item = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("tutorial_item")),
 				ItemList.tutorial_block = new BlockItem(BlockList.tutorial_block, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(BlockList.tutorial_block.getRegistryName()), 
-				ItemList.tutorial_slab = new BlockItem(BlockList.tutorial_slab, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(BlockList.tutorial_slab.getRegistryName())
+				ItemList.tutorial_slab = new BlockItem(BlockList.tutorial_slab, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(BlockList.tutorial_slab.getRegistryName()), 
+				ItemList.tutorial_stair = new BlockItem(BlockList.tutorial_stair, new Item.Properties().group(ItemGroup.MISC)).setRegistryName(BlockList.tutorial_stair.getRegistryName())
 		);
 	}
 	
@@ -36,7 +38,8 @@ public class RegistryEvents {
 	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
 		event.getRegistry().registerAll(
 				BlockList.tutorial_block = new Block(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(2, 6)).setRegistryName(location("tutorial_block")), 
-				BlockList.tutorial_slab = new SlabBlock(Block.Properties.from(BlockList.tutorial_block)).setRegistryName(location("tutorial_slab"))
+				BlockList.tutorial_slab = new SlabBlock(Block.Properties.from(BlockList.tutorial_block)).setRegistryName(location("tutorial_slab")),
+				BlockList.tutorial_stair = new CustomStairsBlock(BlockList.tutorial_block.getDefaultState(), Block.Properties.from(BlockList.tutorial_block)).setRegistryName(location("tutorial_stairs"))
 		);
 	}
 	
---
+++ WillsMod/src/main/java/com/wnorton/willsmod/objects/blocks/CustomStairsBlock.java
@@ -0,0 +1,12 @@
+package com.wnorton.willsmod.objects.blocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+public class CustomStairsBlock extends StairsBlock{
+
+	public CustomStairsBlock(BlockState state, Properties properties) {
+		super(state, properties);
+	}
+	
+}
